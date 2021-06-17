81313039e3f6b47b1a25308762ea64bfdea5c0cd
Amit Limba <amit11794@gmail.com>
2021-06-17 11:58:33 +0530

    changes in "the_ledger_co" as per geektrust guidelines

--- the_ledger_co.cs
+++ the_ledger_co.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace geektrust
+namespace geektrust_the_ledger_co
 {
-    class The_Ledger_Co
+    class Commands
     {
-        public void Run(string[] args)
+        public const string LOAN = "LOAN";
+        public const string PAYMENT = "PAYMENT";
+        public const string BALANCE = "BALANCE";
+    }
+    class the_ledger_co
+    {
+        public static void Main(string[] args)
         {
             //string input = File.ReadAllText(args[0]);
 
             string path = AppDomain.CurrentDomain.BaseDirectory;
-            string file = Path.Combine(path, "Input1.txt"); // copy the file from "The Ledger Co\Input" folder to bin
+            string file = Path.Combine(path, "input1.txt"); // copy the file from "input" folder to bin
             string input = File.ReadAllText(file);
 
             string[] lines = input.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-
             List<Loan> lstLoan = new List<Loan>();
             List<Payment> lstPayment = new List<Payment>();
 
@@ -27,45 +32,18 @@ namespace geektrust
                 switch (lstInput[0])
                 {
                     case Commands.LOAN:
-
                         lstLoan.Add(new Loan(lstInput[1], lstInput[2], Convert.ToInt64(lstInput[3]), Convert.ToInt32(lstInput[4]), Convert.ToInt32(lstInput[5])));
-
                         break;
 
                     case Commands.PAYMENT:
-
                         lstPayment.Add(new Payment(lstInput[1], lstInput[2], Convert.ToInt64(lstInput[3]), Convert.ToInt32(lstInput[4])));
-
                         break;
 
                     case Commands.BALANCE:
-
-                        string BANK_NAME = lstInput[1];
-                        string BORROWER_NAME = lstInput[2];
-                        int EMI_NO = Convert.ToInt32(lstInput[3]);
-
-                        Loan loan = lstLoan.Where(l => l.BANK_NAME.Equals(BANK_NAME) && l.BORROWER_NAME.Equals(BORROWER_NAME)).FirstOrDefault();
-
-                        float AMOUNT_REPAY = loan.GetAmountToBePaidToBank();
-
-                        int NO_OF_EMIS = loan.NO_OF_YEARS * 12;
-                        int AMOUNT_PER_EMI = Convert.ToInt32(Math.Ceiling(AMOUNT_REPAY / NO_OF_EMIS));
-
-                        List<Payment> payments = lstPayment.Where(l => l.BANK_NAME.Equals(BANK_NAME) && l.BORROWER_NAME.Equals(BORROWER_NAME) && l.EMI_NO <= EMI_NO).ToList();
-
-                        long LUMP_SUM_AMOUNT_PAID = payments.Sum(p => p.LUMP_SUM_AMOUNT);
-                        long TOTAL_AMOUNT_PAID = LUMP_SUM_AMOUNT_PAID + (AMOUNT_PER_EMI * EMI_NO);
-
-                        if (TOTAL_AMOUNT_PAID > AMOUNT_REPAY)   // manages the case if last EMI is more than the remaining amount to pay
-                        {
-                            TOTAL_AMOUNT_PAID = Convert.ToInt64(Math.Ceiling(AMOUNT_REPAY));
-                        }
-
-                        float AMOUNT_DUE = AMOUNT_REPAY - TOTAL_AMOUNT_PAID;
-                        int NO_OF_EMIS_LEFT = Convert.ToInt32(Math.Ceiling(AMOUNT_DUE / AMOUNT_PER_EMI));
-
-                        Console.Out.WriteLine(string.Format("{0} {1} {2} {3}", BANK_NAME, BORROWER_NAME, TOTAL_AMOUNT_PAID, NO_OF_EMIS_LEFT));
-
+                        long TOTAL_AMOUNT_PAID = 0;
+                        int NO_OF_EMIS_LEFT = 0;
+                        GetBalanceDetails(lstInput, lstLoan, lstPayment, ref TOTAL_AMOUNT_PAID, ref NO_OF_EMIS_LEFT);
+                        Console.Out.WriteLine(string.Format("{0} {1} {2} {3}", lstInput[1], lstInput[2], TOTAL_AMOUNT_PAID, NO_OF_EMIS_LEFT));
                         break;
 
                     default:
@@ -73,14 +51,34 @@ namespace geektrust
                 }
             }
         }
-    }
-    static class Commands
-    {
-        public const string LOAN = "LOAN";
-        public const string PAYMENT = "PAYMENT";
-        public const string BALANCE = "BALANCE";
-    }
 
+        private static void GetBalanceDetails(List<string> lstInput, List<Loan> lstLoan, List<Payment> lstPayment, ref long TOTAL_AMOUNT_PAID, ref int NO_OF_EMIS_LEFT)
+        {
+            string BANK_NAME = lstInput[1];
+            string BORROWER_NAME = lstInput[2];
+            int EMI_NO = Convert.ToInt32(lstInput[3]);
+
+            Loan loan = lstLoan.Where(l => l.BANK_NAME.Equals(BANK_NAME) && l.BORROWER_NAME.Equals(BORROWER_NAME)).FirstOrDefault();
+
+            float AMOUNT_REPAY = loan.GetAmountToBePaidToBank();
+
+            int NO_OF_EMIS = loan.NO_OF_YEARS * 12;
+            int AMOUNT_PER_EMI = Convert.ToInt32(Math.Ceiling(AMOUNT_REPAY / NO_OF_EMIS));
+
+            List<Payment> payments = lstPayment.Where(l => l.BANK_NAME.Equals(BANK_NAME) && l.BORROWER_NAME.Equals(BORROWER_NAME) && l.EMI_NO <= EMI_NO).ToList();
+
+            long LUMP_SUM_AMOUNT_PAID = payments.Sum(p => p.LUMP_SUM_AMOUNT);
+            TOTAL_AMOUNT_PAID = LUMP_SUM_AMOUNT_PAID + (AMOUNT_PER_EMI * EMI_NO);
+
+            if (TOTAL_AMOUNT_PAID > AMOUNT_REPAY)   // manages the case if last EMI is more than the remaining amount to pay
+            {
+                TOTAL_AMOUNT_PAID = Convert.ToInt64(Math.Ceiling(AMOUNT_REPAY));
+            }
+
+            float AMOUNT_DUE = AMOUNT_REPAY - TOTAL_AMOUNT_PAID;
+            NO_OF_EMIS_LEFT = Convert.ToInt32(Math.Ceiling(AMOUNT_DUE / AMOUNT_PER_EMI));
+        }
+    }
     class Loan
     {
         public Loan(string BANK_NAME, string BORROWER_NAME, long PRINCIPAL, int NO_OF_YEARS, int RATE_OF_INTEREST)
@@ -110,7 +108,6 @@ namespace geektrust
         public int NO_OF_YEARS;
         public int RATE_OF_INTEREST;
     }
-
     class Payment
     {
         public Payment(string BANK_NAME, string BORROWER_NAME, long LUMP_SUM_AMOUNT, int EMI_NO)
